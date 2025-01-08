15b862ff7001e1883a033011d7c19c1bf9e3cdad
Petteri Hallipelto <hpetteri@ad.helsinki.fi>
2025-01-08 10:08:45 +0200

    Exercise 1.9 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -25,10 +25,18 @@ const App = () => {
 const Statistics = ({N_of_good, N_of_neutral, N_of_negative}) => {
   const total = N_of_good + N_of_neutral + N_of_negative
   const positive_percent = N_of_good/total * 100
+  if (total == 0){
+    return (
+      <div>
+          <h1>Statistics</h1>
+          <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
-      
-      <h1>Statistics</h1>
+
+    <h1>Statistics</h1>
     <p>Good: {N_of_good}</p>
     <p>Neutral: {N_of_neutral}</p>
     <p>Negative: {N_of_negative}</p>
