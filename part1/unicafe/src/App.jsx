cd5825a63aa1b51172b178af05b6ba2913a3232f
Petteri Hallipelto <hpetteri@ad.helsinki.fi>
2025-01-08 10:21:34 +0200

    exercise 1.10 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -12,9 +18,10 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>negative</button>
+      <Button handleClick={() => setGood(good + 1)} text={"Good"}></Button>
+      <Button handleClick={() => setNeutral(neutral + 1)} text={"Neutral"}></Button>
+      <Button handleClick={() => setBad(bad + 1)} text={"Negative"}></Button>
+    
       <Statistics N_of_good={good} N_of_negative={bad} N_of_neutral={neutral} />
 
     </div>
@@ -37,14 +44,21 @@ const Statistics = ({N_of_good, N_of_neutral, N_of_negative}) => {
     <div>
 
     <h1>Statistics</h1>
-    <p>Good: {N_of_good}</p>
-    <p>Neutral: {N_of_neutral}</p>
-    <p>Negative: {N_of_negative}</p>
-    <p>all: {total}</p>
-    <p>average: {(N_of_good-N_of_negative)/total}</p>
-    <p>positive: {positive_percent} %</p>
+    <StatisticLine text="Good" value={N_of_good}/>
+    <StatisticLine text="Neutral" value={N_of_neutral}/>
+    <StatisticLine text="Negative" value={N_of_negative}/>
+    <StatisticLine text="All" value={total}/>
+    <StatisticLine text="Average" value={(N_of_good-N_of_negative)/total}/>
+    <StatisticLine text="Positive " value={positive_percent + " %"}/>
   </div>
   )
 }
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <p>{text}: {value}</p>
+  )
+}
+
 
 export default App
