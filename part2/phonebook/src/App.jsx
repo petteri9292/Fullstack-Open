cabb499c89737a0857ed355e81068a1fc65d19c3
Petteri Hallipelto <hpetteri@ad.helsinki.fi>
2025-01-18 14:32:00 +0200

    Exercise 2.11 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios"
 
 
 import Render from "./components/Render.jsx"
@@ -9,15 +10,31 @@ const App = () => {
   // const [persons, setPersons] = useState([
   //   { name: 'Arto Hellas' , id:1, number: "040-1234567"}
   // ]) 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+
+  // const [persons, setPersons] = useState([
+  //   { name: 'Arto Hellas', number: '040-123456', id: 1 },
+  //   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+  //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  // ])
+
+  const [persons,setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState("")
   const [searchTerm, setSearchTerm] = useState("")
+
+
+  const hook = () => {
+    axios.get("http://localhost:3001/persons")
+    .then(response => {
+      console.log(response.data)
+      setPersons(response.data)
+    })
+
+
+  }
+
+  useEffect(hook,[])
 
   const handleNameChange = (event) => {
     // console.log(event.target.value)
